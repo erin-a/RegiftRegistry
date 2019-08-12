cc768d27d3ce5806baa42a32f32fb799ae69ef45
erin-a <erineadler@gmail.com>
2019-08-11 22:33:45 -0500

    Added Psuedo Code Page Structure, pushing all files

--- Regift-Registry/src/main/java/org/launchcode/RegiftRegistry/controllers/HomeController.java
+++ Regift-Registry/src/main/java/org/launchcode/RegiftRegistry/controllers/HomeController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World Regift Registry";
+    public String index(HttpServletRequest request) {
+
+        String name = request.getParameter("name")
+
+        return "Hello " + name + "Regift Registry";
+
     }
 
     @RequestMapping(value = "goodbye")
@@ -18,6 +24,41 @@ public class HomeController {
     public String goodbye() {
         return "Goodbye";
     }
+
+    //pages to create
+
+    //welcome page logged in
+
+    //welcome page logged out
+
+    //create account page
+
+    //add a custom condition page
+
+    //remove a custom condition page
+
+    //add item page
+
+    //edit item page
+
+    //remove item page
+
+    //view/manage registry page owner
+
+    //view registry page guest
+
+    //data fields for item
+        //name
+        //image link
+        //item link
+        //condition
+        //source
+        //accuracy
+        //notes
+
+
+
+
 
 
 }
