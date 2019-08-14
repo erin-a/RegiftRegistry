af768ed3b3c203745faed54c61bad2db052bd26e
erin-a <erineadler@gmail.com>
2019-08-13 19:24:36 -0500

    Adding omitted files to git

---
+++ Regift-Registry/src/main/java/org/launchcode/RegiftRegistry/RegiftRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.RegiftRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiftRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiftRegistryApplication.class, args);
+	}
+
+}
